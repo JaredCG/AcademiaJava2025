1092c30adb28b5415152fd9346135df5a0cda102
JaredCG <87095488+JaredCG@users.noreply.github.com>
2025-02-20 17:56:26 -0600

    Semana6 Swagger 3 (OpenAPI) habilitado

--- Semana6/proyecto.v3/src/main/java/s6/academy/proyecto/v3/Application.java
+++ Semana6/proyecto.v3/src/main/java/s6/academy/proyecto/v3/Application.java
@@ -3,7 +3,7 @@ package s6.academy.proyecto.v3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EnableSwagger2
 @SpringBootApplication
